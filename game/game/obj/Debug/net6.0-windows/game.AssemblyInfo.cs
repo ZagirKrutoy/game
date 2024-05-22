1803c028b247aacd7ce45b1268b3d6212e228d77
Zagir <150962613+ZagirKrutoy@users.noreply.github.com>
2024-05-22 20:32:42 +0500

    update
    update

--- game/game/obj/Debug/net6.0-windows/game.AssemblyInfo.cs
+++ game/game/obj/Debug/net6.0-windows/game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8be08b21a2ccf3b303cea74eb03b421c214221a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df677e45938f86faf4286ffe15efecf6588dfb6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
